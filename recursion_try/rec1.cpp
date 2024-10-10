19c2842e1ba7047c7bc2754f34aa4a1d87dba1b6
Kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2024-10-10 17:50:56 +0530

    upload first file

---
+++ recursion_try/rec1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+//function to print number from count to 10
+void counting(int count){
+    if(count == 11) return;
+    cout<<count<<endl;
+    counting(++count);
+}
+
+//function to print my name n number of times;          **O(N)**
+
+void printName(int count){
+   if(count==0) return;
+    cout<<"Kamaljeet Yadav"<<endl;
+    printName(--count);
+}
+
+//Function to print from 1 to n (By BackTracking):               **O(N)**
+
+void print1ToN(int count){
+    if(count==0) return;
+    print1ToN(--count);
+    cout<<count+1<<endl;
+}
+
+//function to print from N to 1 (By BackTracking):                 **O(N)** 
+void printNTo1(int count){
+    if(count==0) return;
+    cout<<count<<endl;
+    printNTo1(--count);
+}
+//function to print sum of 1 to n numbers :
+
+void Sum(int count,int sum){
+    if(count==0) {
+        cout<<"sum : "<<sum<<endl;
+        return;
+        }
+    sum+=count;
+    Sum(--count,sum);
+}
+
+int main(){
+    int count,sum=0;
+    cin>>count;
+    //printNTo1(count);
+
+    //printName(count);
+    
+    //print1ToN(count);
+    
+    Sum(count,sum);
+
+    
+
+    cout<<"Hello Program ends here.";
+    return 0;
+}
