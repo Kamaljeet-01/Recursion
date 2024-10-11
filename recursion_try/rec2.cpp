06b7c3ee56317ef04439072130d798df1db2b1a0
Kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2024-10-12 01:01:55 +0530

    Add fabonacci number and palindrome number question

--- recursion_try/rec2.cpp
+++ recursion_try/rec2.cpp
@@ -28,11 +28,24 @@ void printArray(int arr[],int size){
     }
     cout<<endl;
 }
+
+bool palindrome(char arr[],int i,int n){
+    if(i>=n/2) return true;
+    if(arr[i] != arr[n-i-1]) return false;
+    palindrome(arr,i+1,n);
+}
+int fab(int n){
+    if(n<=1) return n;
+    return (fab(n-1)+fab(n-2));
+}
 int main(){
     int arr[5] = {2,4,6,8,10};
+    char arr2[5]={'m','a','d','s','m'};
     //reverse(arr,0,4);
-    rev(arr,5,0);
-    printArray(arr,5);
+    //rev(arr,5,0);
+    //printArray(arr,5);
+    //(palindrome(arr2,0,5))?cout<<"True"<<endl:cout<<"False"<<endl;
+    cout<<fab(4)<<endl;
     cout<<"Program ends here....."<<endl;
     return 0;
 }
